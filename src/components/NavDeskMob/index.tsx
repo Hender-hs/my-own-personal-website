5a979dce870dcace886fc09c862b815bbfcf48c2
Henderson <Henderson.f.p@hotmail.com>
2021-08-15 10:40:31 -0300

    feature: mobile menu toggle is finished

--- src/components/NavDeskMob/index.tsx
+++ src/components/NavDeskMob/index.tsx
@@ -1,23 +1,55 @@
-import * as S from './styles'
+import * as S                           from './styles'
 import { AiFillCaretDown as DownArrow } from 'react-icons/ai'
+import { useState }                     from 'react'
 
 export const Nav = () => {
 
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
-    <S.Nav>
-      <S.H1>.Portifolio</S.H1>
-      <S.H3 className='first-link' >Home</S.H3>
-      <S.H3>Knowledge</S.H3>
-      <S.H3>Projects</S.H3>
-      <S.LangMenu>
-        <S.H3>Language <DownArrow color='white' /> </S.H3>
-        <ul>
-          <li>Portuguese</li>
-          <li>English</li>
-          <li>Spanish</li>
-        </ul>
-      </S.LangMenu>
-      <S.H3 className='last-link' >About me</S.H3>
-    </S.Nav>
+    <>
+      <S.Nav>
+        <S.H1><a href='#'>.Portifolio</a></S.H1>
+        <S.H3 className='first-link' ><a href='#'>Home</a></S.H3>
+        {/* <S.H3><a href='#services'>Knowledge</a></S.H3> */}
+        <S.H3><a href='#services'>Services</a></S.H3>
+        <S.H3><a href='#projects'>Projects</a></S.H3>
+        <S.LangMenu>
+          <S.H3>Language <DownArrow color='white' /> </S.H3>
+          <ul>
+            <li>Portuguese</li>
+            <li>English</li>
+            <li>Spanish</li>
+          </ul>
+        </S.LangMenu>
+        <S.H3 className='last-link' ><a href='#about-me'>About me</a></S.H3>
+      </S.Nav>
+      <S.NavMobile>
+        <S.PositionHeaderItems>
+          <S.MenuBurguerStyled color='white' size='50' onClick={() => setOpenMenu(true)} />
+          <S.H1Mobile><a href='#'>.Portifolio</a></S.H1Mobile>
+        </S.PositionHeaderItems>
+        <S.MenuModal openMenu={openMenu} >
+          <S.PositionalHeaderMenuMobileItems>
+            <S.MenuBurguerStyled color='white' size='50' onClick={() => setOpenMenu(false)} />
+            <S.H1Mobile><a href='#'>.Portifolio</a></S.H1Mobile>
+          </S.PositionalHeaderMenuMobileItems>
+          <S.PositionalModalMobileItems>
+            <S.H3Mobile><a onClick={() => setOpenMenu(false)} href='#'>Home</a></S.H3Mobile>
+            <S.H3Mobile><a onClick={() => setOpenMenu(false)} href='#services'>Services</a></S.H3Mobile>
+            <S.H3Mobile><a onClick={() => setOpenMenu(false)} href='#projects'>Projects</a></S.H3Mobile>
+            {/* <S.LangMenu>
+              <S.H3>Language <DownArrow color='white' /> </S.H3>
+              <ul>
+                <li>Portuguese</li>
+                <li>English</li>
+                <li>Spanish</li>
+              </ul>
+            </S.LangMenu> */}
+            <S.H3Mobile className='last-link' ><a href='#about-me'>About me</a></S.H3Mobile>
+          </S.PositionalModalMobileItems>
+        </S.MenuModal>
+      </S.NavMobile>
+    </>
   )
 }
--- src/components/NavDeskMob/styles.ts
+++ src/components/NavDeskMob/styles.ts
@@ -1,4 +1,10 @@
-import styled from 'styled-components'
+import { IoMdMenu as BurguerMenu }  from 'react-icons/io'
+import styled                       from 'styled-components'
+
+interface MobileModal {
+  openMenu: boolean
+}
+
 
 export const Nav = styled.nav `
   /* position: relative; */
@@ -20,6 +26,11 @@ export const Nav = styled.nav `
   @media (max-width: 768px) {
     display: none;
   }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `
 
 export const H1 = styled.h1 `
@@ -27,7 +38,6 @@ export const H1 = styled.h1 `
   font-size: 40px;
   font-weight: 100;
   padding: 0 0 0 25px;
-  cursor: pointer;
 `
 
 export const DivLinks = styled.div `
@@ -83,4 +93,85 @@ export const LangMenu = styled.div `
       color: gray;
     }
   }
+`
+
+export const NavMobile = styled.nav `
+  background-color: var(--header-cover-color);
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+export const MenuBurguerStyled = styled(BurguerMenu) `
+  cursor: pointer;
+` 
+
+export const PositionHeaderItems = styled.div `
+  width: 95%;
+  display: flex;
+`
+
+export const H1Mobile = styled.h1 `
+  color: white;
+  font-size: 40px;
+  font-weight: 100;
+  padding: 0 0 0 25px;
+  margin-left: auto;
+`
+
+export const MenuModal = styled.div<MobileModal> `
+  background-color: var(--custom-black);
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  visibility: ${({openMenu}) => openMenu ? 'visible' : 'hidden'};
+  z-index: 2;
+  overflow: hidden;
+  opacity: ${({openMenu}) => openMenu ? '1' : '0'};
+  transform: translateX(${({openMenu}) => openMenu ? '0' : '-100vw'});
+  transition: .9s;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const PositionalHeaderMenuMobileItems = styled.div `
+  width: 95%;
+  display: flex;
+  align-items: center;
+  height: 62px;
+
+  svg {
+    margin-top: 12px;
+  }
+`
+
+export const PositionalModalMobileItems = styled.div `
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+export const H3Mobile = styled.h3 `
+  font-size: 55px;
+  color: white;
+  font-weight: 100;
+  cursor: pointer;
 `
